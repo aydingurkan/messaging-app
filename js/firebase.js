57e19d2b7982f348af270fd80c2527903ddea342
aydingurkan <aydingurkan2003@gmail.com>
2025-05-29 22:19:22 +0300

    fix: databaseURL eklendi

--- js/firebase.js
+++ js/firebase.js
@@ -2,16 +2,16 @@
 const firebaseConfig = {
     apiKey: "AIzaSyD-KbmFtl4rRu7n4EJ2Z180XEPNH7T00w4",
     authDomain: "mesaj-8d504.firebaseapp.com",
+    databaseURL: "https://mesaj-8d504-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "mesaj-8d504",
-    storageBucket: "mesaj-8d504.firebasestorage.app",
+    storageBucket: "mesaj-8d504.appspot.com",
     messagingSenderId: "685536318784",
-    appId: "1:685536318784:web:03759fe9c799d5868456a5",
-    ölçümKimliği: "G-Z8YF2W2VP9"
-    };
+    appId: "1:685536318784:web:03759fe9c799d5868456a5"
+  };
   
   // Firebase'i başlat
   firebase.initializeApp(firebaseConfig);
   
-  // Auth ve Database objelerini tanımla
+  // Firebase servislerini tanımla
   const auth = firebase.auth();
   const db = firebase.database();
